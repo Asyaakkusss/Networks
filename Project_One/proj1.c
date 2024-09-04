140ce9c64124771f994afacb1681745e8e044970
Asya <aya29@case.edu>
2024-09-04 01:23:51 -0400

    start of project

---
+++ Project_One/proj1.c
@@ -0,0 +1,21 @@
+/*
+Name: Asya Akkus 
+Case Network ID: aya29
+File Name: proj1
+Date Created: 9/4/2024
+Description: file parses long lists of IP addresses and 
+determines which ones are valid. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file = fopen("samplec.txt","r"); 
+
+    char buffer[2939]; 
+
+    while (fgets(buffer, sizeof(buffer), file)) {
+
+    }
+}
