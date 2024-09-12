885734e574973a5c81e2d0ce9b7fa617afa85110
Asya Akkus <aya29@case.edu>
2024-09-11 23:16:45 -0400

    updates in case i forgot something

--- Project_One/proj1.c
+++ Project_One/proj1.c
@@ -165,8 +165,104 @@ int main(int argc, char *argv[]) {
 
     }
         
-    if (cmd_line_flags == ARG_LIST+ARG_FILE)
-        fprintf (stdout,"hello world\n");
+    if (cmd_line_flags == ARG_LIST+ARG_FILE) {
+
+            FILE *file = fopen(filename, "r"); 
+
+    // Ensure throwing error if the file is not found 
+    if (file == NULL) {
+        printf("File is not available. Please try another file name\n");
+        return 1;  // Exit with an error code
+    }
+    
+    // Allocate memory for a buffer that reads information from the file into the buffer 
+    char buffer[50]; 
+
+    // Counters for valid and invalid addresses 
+    int valid_counter = 0; 
+    int invalid_counter = 0; 
+
+    // Read each line from the file
+    while (fgets(buffer, sizeof(buffer), file)) {
+        //create a new buffer that will be used to 
+        // Remove trailing newline character, if present
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len - 1] == '\n') {
+            buffer[len - 1] = '\0';
+        }
+
+        // Boolean for is valid 
+        int is_valid = 1; 
+
+        //counter for number of dots 
+        int dot_count = 0; 
+
+        //we check how many periods there are before tokenizing it, so we can take it out of the running 
+        //if things don't add up. There should only be 3 periods in each IP address. 
+        //check to make sure there are only 3 periods in the buffer 
+        for (int i = 0; i < strlen(buffer); i++) {
+            if (buffer[i] == '.')
+                dot_count++; 
+        }
+
+        if (dot_count > 3) {
+            is_valid = 0; 
+        }
+
+        // Tokenize the string based on the dots present 
+        char *pointer = strtok(buffer, "."); 
+
+        // Counter for number of octets
+        int num_octets = 0; 
+
+        while (pointer) {
+            // Check for leading zeroes
+            if (pointer[0] == '\0' || (pointer[0] == '0' && pointer[1] != '\0')) {
+                is_valid = 0; 
+                break; 
+            }
+
+            // Check if the octet contains only digits
+            for (size_t i = 0; i < strlen(pointer); i++) {
+                if (!isdigit((unsigned char)pointer[i])) {
+                    is_valid = 0; 
+                    break;
+                }
+            }
+            if (!is_valid) break;
+
+            // Convert the octet to an integer and validate its range
+            int octet = atoi(pointer); 
+            if (octet > 255 || octet < 0) {
+                is_valid = 0; 
+                break; 
+            }
+
+            // Move on to the next token 
+            pointer = strtok(NULL, "."); 
+            num_octets++; 
+        }
+
+        // Validate the IP address based on the number of octets
+        if (is_valid && num_octets == 4) {
+            valid_counter++; 
+            printf("%s", buffer); 
+            printf("+\n"); 
+        } else {
+            invalid_counter++; 
+            printf("%s", buffer); 
+            printf("-\n"); 
+        }
+    }
+
+    fclose(file); 
+
+    int num_lines = valid_counter + invalid_counter; 
+    printf("LINES: %d\n", num_lines);
+    printf("VALID: %d\n", valid_counter);
+    printf("INVALID: %d\n", invalid_counter);
+    }
+        
     
     if ((cmd_line_flags == 0))
     {
