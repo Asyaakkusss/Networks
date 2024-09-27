7304cba2e64f76d059ba2cfd1422ff347cb78f50
Asya Akkus <aya29@case.edu>
2024-09-26 20:45:20 -0400

    housekeeping is hereeeee

--- Project_Two/proj2.c
+++ Project_Two/proj2.c
@@ -1,44 +1,3 @@
-/*
--i option
-The “-i” option will be used to print debugging information about the given command line parameters to
-standard output (i.e., the screen). When “-i” is given on the command line your program will output the
-following lines:
-INFO: host: [hostname]
-INFO: web_file: [url_filename]
-INFO: output_file: [local_filename]
-The format for these lines must follow these requirements:
-• The “INFO:” must be at the beginning of the line.
-• A single space follows “INFO:”.
-• The labels that appear after “INFO:<space>” must be exactly as they appear above (e.g., using all
-lower case letters).
-• After the label, add a colon (“:”) and then a single space before printing the value.
-• The “[hostname]” value comes from the URL given on the command line, as described in the “-u”
-discussion above.
-• The “[url filename]” value is the filename portion of the URL given on the command line, as described
-in the “-u” discussion above. If no filename is given on the command line, the default filename of “/”
-must be printed.
-• The “[local filename]” value is the name of the file on the local system where the web page at the given
-URL will be stored. This is the filename given with the “-w” option on the command line.
-• The three lines must appear in the order given above.
-• Do not print extra lines—including blank lines.
-• Do not print any extra whitespace before, after or within the output lines.
-• The “-i” output is to be printed regardless of whether there are errors fetching the web page. The “-i”
-output will not be printed if there are errors in the command line options given by the user.
-The following are several illustrative examples with the “-i” option:
-%%% ./proj2 -i -u http://www.icir.org -w testing.html
-INFO: host: www.icir.org
-INFO: web_file: /
-INFO: output_file: testing.html
-%%% ./proj2 -w mallman.html -i -u http://www.icir.org/mallman/
-INFO: host: www.icir.org
-INFO: web_file: /mallman/
-INFO: output_file: mallman.html
-%%% ./proj2 -u http://www.icir.org/mallman/index.html -w /tmp/mallman.html -i
-INFO: host: www.icir.org
-INFO: web_file: /mallman/index.html
-INFO: output_file: /tmp/mallman.html
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -145,7 +104,6 @@ void parseargs (int argc, char *argv []) {
 }
 
 /*for the global variables of host name and url filename*/
-//us this in i option too in order to prevent points off on repeating code 
 void find_host(char *url) {
   const char* start = strstr(url, "://") + 3; 
 
@@ -162,7 +120,6 @@ void find_host(char *url) {
 
 }
 
-//use this in i option to prevent points off on repeating code 
 void find_url_filename(char *url) {
   const char* start = strstr(url, "://") + 3; 
 
@@ -223,12 +180,6 @@ void i_option() {
 
 
 void q_option() {
-
-/*
-REQ: GET [url_filename] HTTP/1.0
-REQ: Host: [hostname]
-REQ: User-Agent: Case CSDS 325/425 WebClient 0.1
-*/
 
 printf("REQ: GET %s HTTP/1.0\r\n", URL_FILENAME); 
 printf("REQ: Host: %s \r\n", HOST_NAME); 
