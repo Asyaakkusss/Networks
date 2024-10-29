359e28a9620c8bd1a6db2ced7de1e479db09ddd6
Asya Akkus <aya29@case.edu>
2024-10-28 22:51:30 -0400

    cleanup for proj3 and makefile/c file created for proj4

---
+++ Project_Four/proj4.c
@@ -0,0 +1,8 @@
+/*
+Name: Asya Akkus 
+Case Network ID: aya29
+File Name: proj4.c
+Date Created: 10/28/2024
+Description: file implements the r, i, s, t, and m options 
+that process a packet trace file in different ways. 
+*/
