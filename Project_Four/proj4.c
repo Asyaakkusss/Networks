9a09d01b7125275557541f3566384288d4438818
Asya Akkus <aya29@case.edu>
2024-10-31 16:35:44 -0400

    start of the i option

--- Project_Four/proj4.c
+++ Project_Four/proj4.c
@@ -21,6 +21,7 @@ that process a packet trace file in different ways.
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <net/ethernet.h>
 
 #define ARG_R  0x1  // Root directory (trace file)
 #define ARG_I  0x2  // Trace information mode
@@ -30,6 +31,33 @@ that process a packet trace file in different ways.
 
 char *trace_file = NULL; 
 unsigned short cmd_line_flags = 0;
+
+#define MAX_PKT_SIZE        1600
+
+/* meta information, using same layout as trace file */
+struct meta_info
+{
+    unsigned int secs;
+    unsigned int usecs;
+    unsigned short caplen;
+    unsigned short ignored;
+};
+
+/* record of information about the current packet */
+struct pkt_info
+{
+    unsigned short caplen;      /* from meta info */
+    double now;                 /* from meta info */
+    unsigned char pkt [MAX_PKT_SIZE];
+    struct ether_header *ethh;  /* ptr to ethernet header, if present,
+                                   otherwise NULL */
+    struct iphdr *iph;          /* ptr to IP header, if present, 
+                                   otherwise NULL */
+    struct tcphdr *tcph;        /* ptr to TCP header, if present,
+                                   otherwise NULL */
+    struct udphdr *udph;        /* ptr to UDP header, if present,
+                                   otherwise NULL */
+};
 
 void usage(char *progname) {
     fprintf(stderr, "%s -r trace_file [-i | -s | -t | -m]\n", progname);
@@ -99,7 +127,121 @@ void parseargs(int argc, char *argv[]) {
     validateargs(argc, argv);
 }
 
+
+unsigned short next_packet (int fd, struct pkt_info *pinfo)
+{
+    struct meta_info meta;
+    int bytes_read;
+
+    memset (pinfo,0x0,sizeof (struct pkt_info));
+    memset (&meta,0x0,sizeof (struct meta_info));
+
+    /* read the meta information */
+    bytes_read = read (fd,&meta,sizeof (meta));
+    if (bytes_read == 0)
+        return (0);
+    if (bytes_read < sizeof (meta))
+        fprintf (stderr, "cannot read meta information");
+    pinfo->caplen = ntohs (meta.caplen);
+    /* TODO: set pinfo->now based on meta.secs & meta.usecs */
+    if (pinfo->caplen == 0)
+        return (1);
+    if (pinfo->caplen > MAX_PKT_SIZE)
+        fprintf(stderr, "packet too big");
+    /* read the packet contents */
+    bytes_read = read (fd,pinfo->pkt,pinfo->caplen);
+    if (bytes_read < 0)
+        fprintf(stderr, "error reading packet");
+    if (bytes_read < pinfo->caplen)
+        fprintf(stderr, "unexpected end of file encountered");
+    if (bytes_read < sizeof (struct ether_header))
+        return (1);
+    pinfo->ethh = (struct ether_header *)pinfo->pkt;
+    pinfo->ethh->ether_type = ntohs (pinfo->ethh->ether_type);
+    if (pinfo->ethh->ether_type != ETHERTYPE_IP)
+        /* nothing more to do with non-IP packets */
+        return (1);
+    if (pinfo->caplen == sizeof (struct ether_header))
+        /* we don't have anything beyond the ethernet header to process */
+        return (1);
+    /* TODO:
+       set pinfo->iph to start of IP header
+       if TCP packet, 
+          set pinfo->tcph to the start of the TCP header
+          setup values in pinfo->tcph, as needed
+       if UDP packet, 
+          set pinfo->udph to the start of the UDP header,
+          setup values in pinfo->udph, as needed */
+    return (1);
+}
+
+/*
+Trace Information Mode
+When your program is invoked with the “-i” option, it will operate in “trace information” mode. This mode
+provides a high-level summary of the trace file. After processing each packet in the trace, your program will
+print several pieces of information to standard output in this format:
+[trace_filename] [first_time] [trace_duration] [total_pkts] [IP_pkts]
+The value of “first time” is the timestamp (found in meta information for each packet in the trace) of the
+first packet in the trace file. This will be printed in seconds-since-epoch format and include 6 decimal places
+of precision—e.g., “1103112609.135350”. The value of “trace duration” is the duration of the trace file—i.e.,
+the timestamp of the last packet in the file minus first time. The duration should be printed in seconds and
+to 6 decimal places of precision. The value of “total pkts” is the number of packets found in the packet
+trace file, reported as a decimal number with no padding. Finally, the value of “ip pkts” is the number of
+IPv4 packets found in the packet trace file, reported as a decimal number with no padding. Note: ip pkts ≤
+total pkts. Also, note that the braces in the above format are to denote variables and should not appear in
+your output.
+The labels will be presented in lower-case, as shown. A single space will separate all fields / words on each
+line. Further, no additional white space at the beginning or end of the lines may be printed.
+Hint: We determine whether a packet is an IPv4 packet or not by consulting the “type” field in the Ethernet
+header. See packet trace format document for more information.
+Sample output:
+./proj4 -r trace1.dmp -i
+trace1.dmp 1103112609.132870 0.002484 4 3
+*/
+
+void i_option() {
+    printf("%s", trace_file); 
+    FILE *f = fopen(trace_file, "rb"); 
+    
+    if (f == NULL) {
+        fprintf(stderr, "error opening the file. Please try a different file"); 
+        exit(1); 
+    }
+
+    struct pkt_info pinfo; 
+
+    int fd = fileno(f); 
+    int total_pkts = 0;
+    int ip_pkts = 0; 
+    double first_time = 0.0; 
+    double last_time = 0.0; 
+
+    while (next_packet(fd, &pinfo)) {
+        if (total_pkts == 0) {
+            first_time = pinfo.now;
+        }
+
+        last_time = pinfo.now;  
+
+        total_pkts++;
+        
+        // Check if the packet is an IP packet
+        if (pinfo.ethh->ether_type == ETHERTYPE_IP) {
+            ip_pkts++;
+        }
+    }
+    
+    double trace_duration = last_time - first_time;
+
+    // Print the results in the specified format
+    printf("%.6f %.6f %d %d\n", first_time, trace_duration, total_pkts, ip_pkts);
+
+    fclose(f);
+}
+
 int main(int argc, char *argv[]) {
     parseargs(argc, argv);
     return 0;
+
+    if ()
 }
